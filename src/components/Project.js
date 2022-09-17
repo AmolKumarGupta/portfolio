36babee46ed3ffefae21594981d00d74808fef4d
amol <amlo1232001@gmail.com>
2022-09-17 19:50:12 +0530

    fix: warnings

--- src/components/Project.js
+++ src/components/Project.js
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import ProjectItems from "./ProjectItems";
 
 const loader = [1,2,3,4].map((ele)=>{
-    return <div key={ele} className="border border-slate-300 rounded-md min-w-[280px] w-[400px] px-3 py-2"><div className="animate-pulse"><a href="" title="loading" target="_blank" rel="noreferrer"><h2 className="text-xl font-medium capitalize bg-slate-200 w-1/2 h-4"></h2></a><p className="text-sm my-2 lh-2 text-grey-400 bg-slate-200 w-100 h-2"></p><p className="text-sm my-2 lh-2 text-grey-400 bg-slate-200 w-4/5 h-2"></p><p className="flex flex-wrap gap-1 my-2"><span className="text-white border border-slate-200 rounded-xl text-xs px-2 bg-slate-200 min-w-[3rem] w-100 h-3"></span><span className="text-white border border-slate-200 rounded-xl text-xs px-2 bg-slate-200 min-w-[3rem] w-100 h-3"></span></p></div></div>;
+    return <div key={ele} className="border border-slate-300 rounded-md min-w-[280px] w-[400px] px-3 py-2"><div className="animate-pulse"><a href="#top" title="loading" target="_blank" rel="noreferrer"><div className="text-xl font-medium capitalize bg-slate-200 w-1/2 h-4"></div></a><p className="text-sm my-2 lh-2 text-grey-400 bg-slate-200 w-100 h-2"></p><p className="text-sm my-2 lh-2 text-grey-400 bg-slate-200 w-4/5 h-2"></p><p className="flex flex-wrap gap-1 my-2"><span className="text-white border border-slate-200 rounded-xl text-xs px-2 bg-slate-200 min-w-[3rem] w-100 h-3"></span><span className="text-white border border-slate-200 rounded-xl text-xs px-2 bg-slate-200 min-w-[3rem] w-100 h-3"></span></p></div></div>;
 });
 
 const Project = ({repos})=> {
