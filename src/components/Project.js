63894ca33a6013a83f7122e67eb60e9d4caa7b98
amol <amlo1232001@gmail.com>
2023-03-31 01:24:39 +0530

    [feat] gist integration

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,31 +1,51 @@
 import { useEffect, useState } from "react";
 import ProjectItems from "./ProjectItems";
+import { useRepoList } from "../utils/hooks/Provider";
 
-const loader = [1,2,3,4].map((ele)=>{
+const loader = [1,2,3].map((ele)=>{
     return <div key={ele} className="border border-slate-300 rounded-md min-w-[280px] w-[400px] px-3 py-2"><div className="animate-pulse"><a href="#top" title="loading" target="_blank" rel="noreferrer"><div className="text-xl font-medium capitalize bg-slate-200 w-1/2 h-4"></div></a><p className="text-sm my-2 lh-2 text-grey-400 bg-slate-200 w-100 h-2"></p><p className="text-sm my-2 lh-2 text-grey-400 bg-slate-200 w-4/5 h-2"></p><p className="flex flex-wrap gap-1 my-2"><span className="text-white border border-slate-200 rounded-xl text-xs px-2 bg-slate-200 min-w-[3rem] w-100 h-3"></span><span className="text-white border border-slate-200 rounded-xl text-xs px-2 bg-slate-200 min-w-[3rem] w-100 h-3"></span></p></div></div>;
 });
 
 const Project = ({repos})=> {
+    const repolist = useRepoList();
     const [listItems, setListItems] = useState([]);
     const [loading,setLoading] = useState(loader);
 
     useEffect( ()=> {
-        if(repos != null) {
-            // console.log(repos);
-            // console.log('list run')
+        if(repos != null && repolist != null) {
+            let repolistContent = null;
 
-            const filteredRepos = repos.filter( (repo) => {
-                return repo.topics.length;
-            });
+            let file_name = process.env.REACT_APP_REPOLIST_NAME;
+            if( repolist?.files && repolist?.files[file_name] ) {
+                repolistContent = repolist?.files[file_name].content;
+                repolistContent = JSON.parse(repolistContent);
+            }
 
-            const items = filteredRepos.map((e, i) => {
-                return <ProjectItems key={i} repo={e}/>
-            });
-            
-            setListItems(items);
-            setLoading(false);
+            if (repolistContent) {
+                const filteredRepos = repos.filter( (repo) => {
+                    return repolistContent.includes(repo.id);
+                });
+                
+                const repoHash = new Map();
+                filteredRepos.forEach(repo => {
+                    repoHash.set(repo.id, repo);
+                });
+                
+                const sortedRepos = [];
+                repolistContent.forEach(repo => {
+                    sortedRepos.push(repoHash.get(repo));
+                });
+                
+                
+                const items = sortedRepos.map((e, i) => {
+                    return <ProjectItems key={i} repo={e}/>
+                });
+                
+                setListItems(items);
+                setLoading(false);
+            }
         }
-    }, [repos]);
+    }, [repos, repolist]);
 
     return (
         <>
--- src/utils/hooks/Provider.js
+++ src/utils/hooks/Provider.js
@@ -7,10 +7,63 @@ export default function useUserProvider() {
         let signal = true;
 
         if(signal) {
-            // console.log('useUserprovider effect run');
-            fetch(process.env.REACT_APP_API + 'users/amolkumargupta/repos')
-            .then( (res)=> { return res.json()} )
-            .then( (data)=> { setUser(data) } )
+            let storedJson = localStorage.getItem('repos');
+            let data = null;
+            if ( storedJson ) {
+                storedJson = JSON.parse(storedJson);
+                if (storedJson.timestamp > Date.now()) {
+                    data = storedJson.data;
+                }
+            }
+            if (data) {
+                setUser(storedJson.data);
+            }else {
+                fetch(process.env.REACT_APP_API + 'users/amolkumargupta/repos')
+                .then( (res)=> { return res.json()} )
+                .then( (data)=> {
+                    if (! data.message) {
+                        localStorage.setItem('repos', JSON.stringify({timestamp: Date.now()+(30*60*1000), data}));
+                    }
+                    setUser(data);
+                })
+            }
+        }
+
+        return () => {
+            signal = false;
+        } 
+    }, []);
+
+    return user;
+}
+
+export function useRepoList() {
+    const [user, setUser] = useState(null);
+
+    useEffect(() => {
+        let signal = true;
+
+        if(signal) {
+            let storedJson = localStorage.getItem('repolist');
+            let data = null;
+            if ( storedJson ) {
+                storedJson = JSON.parse(storedJson);
+                if (storedJson.timestamp > Date.now()) {
+                    data = storedJson.data;
+                }
+            }
+            if (data) {
+                setUser(storedJson.data);
+            }else {
+                fetch(process.env.REACT_APP_API + 'gists/' + process.env.REACT_APP_REPOLIST_ID)
+                .then( (res)=> { return res.json()} )
+                .then( (data)=> {
+                    if (! data.message) {
+                        localStorage.setItem('repolist', JSON.stringify({timestamp: Date.now()+(30*60*1000), data}));
+                    }
+                    setUser(data)
+                })
+            }
         }
 
         return () => {
