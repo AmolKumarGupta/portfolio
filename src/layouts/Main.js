c860c950d4fdc98a3dc8fa871ab8e9282a35ad11
amol <amlo1232001@gmail.com>
2024-04-07 15:37:26 +0530

    fix: update icons

--- src/layouts/Main.js
+++ src/layouts/Main.js
@@ -2,8 +2,8 @@ export default function Main() {
     return (
         <main className="container mx-auto grow flex px-8 sm:px-4">
             <div className="w-fit m-auto text-4xl sm:text-6xl md:text-8xl">
-                <h1>Amol Kumar Gupta</h1>
-                <p className="text-[#7689de]">Web developer</p>
+                <h1 className="text-center">Amol Kumar Gupta</h1>
+                <p className="text-[#7689de] text-center">Web developer</p>
             </div>
         </main>
     );
