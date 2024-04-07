a8f328cfe1b064d9edc7015cd4e4f67dd8dfbecb
amol <amlo1232001@gmail.com>
2024-04-07 15:24:27 +0530

    feat: add color to subheading

--- src/layouts/Main.js
+++ src/layouts/Main.js
@@ -3,7 +3,7 @@ export default function Main() {
         <main className="container mx-auto grow flex px-8 sm:px-4">
             <div className="w-fit m-auto text-4xl sm:text-6xl md:text-8xl">
                 <h1>Amol Kumar Gupta</h1>
-                <p className="hover:text-[#7689de]">Web developer</p>
+                <p className="text-[#7689de]">Web developer</p>
             </div>
         </main>
     );
